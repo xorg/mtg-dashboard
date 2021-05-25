3fb3fb77d7796eb14dbd65df3bbc5fd8300cbec5
Stefan Schneider <mail@stefanschneider.me>
2021-05-25 21:29:06 +0200

    Fix packaging error in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setuptools.setup(
     name='mtg-dashboard',
     version='0.0.1',
     long_description=__doc__,
-    packages = setuptools.find_packages(),
+    packages = ['app'],
     include_package_data=True,
     zip_safe=False,
-    install_requires = ["Flask"]
+    install_requires = ['app']
 )
 
