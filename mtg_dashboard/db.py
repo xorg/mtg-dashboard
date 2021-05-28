02f28a6f6701a2ee70b5fe088cb8f6eb17cb008f
Stefan Schneider <mail@stefanschneider.me>
2021-05-29 00:40:51 +0200

    Add module for db operations

---
+++ mtg_dashboard/db.py
@@ -0,0 +1,16 @@
+from mtg_dashboard.models import db
+from flask import Blueprint
+
+db_bp = Blueprint('db', __name__)
+
+
+@db_bp.cli.command("drop")
+def drop():
+    """Drop all database tables"""
+    db.drop_all()
+
+
+@db_bp.cli.command("init")
+def init():
+    """Drop all database tables"""
+    db.create_all()
