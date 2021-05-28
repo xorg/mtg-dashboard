dc73c66e0d7ff0cc47a5c7bff32f873d0b2b9878
Stefan Schneider <mail@stefanschneider.me>
2021-05-28 23:36:18 +0200

    Fix circular import hell and switch to app factory

--- mtg_dashboard/__init__.py
+++ mtg_dashboard/__init__.py
@@ -1,33 +1,27 @@
 import os
-
 from flask import Flask
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "flask.sqlite"),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route("/hello")
-    def hello():
-        env_var = os.environ.get("TEST_VAR")
-        return f"Testing env vars: {env_var}"
-
+def create_app():
+    app = Flask(__name__)
+    env = os.environ.get("FLASK_ENV")
+    app.config.from_object(f'mtg_dashboard.config.{env.capitalize()}')
+    register_extensions(app)
+    register_blueprints(app)
     return app
+
+
+def register_extensions(app):
+    from .models import db
+    db.init_app(app)
+
+
+def register_blueprints(app):
+    from .crawler import crawler_bp
+    from .views import views_bp
+
+    app.register_blueprint(crawler_bp)
+    app.register_blueprint(views_bp)
+
+
+app = create_app()
