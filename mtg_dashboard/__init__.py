b4e7037ac30027ba9c5f29519cc68d2eb9f489f1
Stefan Schneider <mail@stefanschneider.me>
2021-06-02 18:08:06 +0200

    Enable cors for angular

--- mtg_dashboard/__init__.py
+++ mtg_dashboard/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     env = os.environ.get("FLASK_ENV")
     app.config.from_object(f'mtg_dashboard.config.{env.capitalize()}')
     register_extensions(app)
