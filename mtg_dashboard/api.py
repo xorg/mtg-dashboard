c901ba43a8430c6a5eb7e23be04221b8c5f3ffc4
Stefan Schneider <mail@stefanschneider.me>
2021-08-19 22:03:28 +0200

    Implement price, price history, current_price and value of collections
    This commit adds the possibility to display the price history of each
    collection as well as calculate the total value of a collection as well
    as the value history

--- mtg_dashboard/api.py
+++ mtg_dashboard/api.py
@@ -1,22 +1,38 @@
 from flask import Blueprint
 from flask import jsonify
-from mtg_dashboard.models import Collection, Price, Card
+from mtg_dashboard.models import Collection, Price, Card, db
 
 api_bp = Blueprint("api", __name__)
 
 
 # view all collections
-@api_bp.route("/api/collection", methods=["GET"])
+@api_bp.route("/api/collections", methods=["GET"])
 def collections():
     collections = Collection.query.all()
     return jsonify(collections)
 
 
+# view price history of one collection
+@api_bp.route("/api/collections/<int:id>", methods=["GET"])
+def collection_detail(id):
+    collection = Collection.query.get(id)
+    query = collection.value_history()
+    res = db.session.execute(query)
+    value_history = [tuple(i) for i in res]
+    col = {
+        "name": collection.name,
+        "value": collection.value,
+        "history": value_history,
+        "cards": list(collection.cards),
+    }
+    return jsonify(col)
+
+
 # view all cards
-@api_bp.route("/api/card", methods=["GET"])
+@api_bp.route("/api/cards", methods=["GET"])
 def cards():
-    cards = Card.query.all()
-    return jsonify(cards)
+    cards = Card.query.filter(Card.current_price > 10.0)
+    return jsonify(list(cards))
 
 
 # specific card with prices
@@ -29,13 +45,9 @@ def card(id):
 
 
 # view prices of a card by id
-@api_bp.route("/api/card/<int:card_id>/price", methods=["GET"])
+@api_bp.route("/api/cards/<int:card_id>/price", methods=["GET"])
 def card_prices(card_id):
-    card_price_query = Price.query.filter_by(card_id=card_id).order_by(Price.date.asc()).all()
+    card_price_query = (
+        Price.query.filter_by(card_id=card_id).order_by(Price.date.asc()).all()
+    )
     return jsonify(card_price_query)
-
-
-@api_bp.route("/api/price", methods=["GET"])
-def price():
-    price = Price.query.order_by(Price.price.desc()).all()
-    return jsonify(price)
--- mtg_dashboard/models.py
+++ mtg_dashboard/models.py
@@ -3,6 +3,11 @@ from dataclasses import dataclass
 from typing import List
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import event
+from sqlalchemy.orm import object_session
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import select, func
+
 
 db = SQLAlchemy()
 
@@ -17,78 +22,6 @@ collections = db.Table(
 
 
 @dataclass
-class Card(db.Model):
-    id: int
-    name: str
-    setname: str
-    count: int
-    img: str
-    prices = List["Price"]
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    img = db.Column(db.String(255), nullable=True)
-    name = db.Column(db.String(80), nullable=False)
-    setname = db.Column(db.String(5), nullable=True)
-    count = db.Column(db.Integer, nullable=False, default=1)
-
-    def get_current_price(self):
-        return self.prices[0]
-
-    def serialize(self):
-        """Returns a dictionary with
-        additional information not saved in database"""
-        dct = dict()
-        dct["id"] = self.id
-        dct["name"] = self.name
-        dct["setname"] = self.setname
-        dct["current_price"] = self.get_current_price()
-        dct["count"] = self.count
-        dct["img"] = self.img
-        return dct
-
-    def __repr__(self):
-        if self.setname:
-            return f"{self.count}x {self.name} ({self.setname})"
-        return f"{self.name}"
-
-
-@dataclass
-class Collection(db.Model):
-    id: int
-    name: str
-    cards = List["Card"]
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
-    value = db.Column(db.Float())
-
-    cards = db.relationship(
-        "Card",
-        secondary=collections,
-        lazy="subquery",
-        backref=db.backref("collections", lazy=True),
-    )
-
-    def get_collection_value(self):
-        value = 0
-        for card in self.cards:
-            value += card.get_current_price()
-
-    def serialize(self):
-        """Returns a dictionary with
-        additional information not saved in database"""
-        dct = dict()
-        dct["cards"] = self.cards
-        dct["number of cards"] = len(self.cards)
-        dct["name"] = self.name
-        dct["id"] = self.id
-        return dct
-
-    def __repr__(self):
-        return f"Collection {self.name} ({len(self.cards)} cards)"
-
-
-@dataclass
 class Price(db.Model):
     id: int
     price: float
@@ -100,7 +33,7 @@ class Price(db.Model):
     card_id = db.Column(db.Integer, db.ForeignKey("card.id"), nullable=False)
     card = db.relationship(
         "Card",
-        backref=db.backref("prices", lazy=True, order_by="Price.date.asc()"),
+        backref=db.backref("prices", lazy=True, order_by="Price.date.desc()"),
     )
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
 
@@ -108,13 +41,82 @@ class Price(db.Model):
         return f"Price: {self.date}: {self.card.name} {self.price} "
 
 
+@dataclass
+class Card(db.Model):
+    current_price: float
+    id: int
+    name: str
+    setname: str
+    count: int
+    img: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    img = db.Column(db.String(255), nullable=True)
+    name = db.Column(db.String(80), nullable=False)
+    setname = db.Column(db.String(5), nullable=True)
+    count = db.Column(db.Integer, nullable=False, default=1)
+
+    @hybrid_property
+    def current_price(self):
+        return self.prices[0].price
+
+    @current_price.expression
+    def current_price(cls):
+        return select([Price.price]).where(Price.card_id == cls.id).limit(1).as_scalar()
+
+    def __repr__(self):
+        if self.setname:
+            return f"{self.count}x {self.name} ({self.setname})"
+        return f"{self.name}"
+
+
+@dataclass
+class Collection(db.Model):
+    value: float
+    # cards: List["Card"]
+    id: int
+    name: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    cards = db.relationship(
+        "Card",
+        secondary=collections,
+        lazy="subquery",
+        backref=db.backref("collections", lazy=True),
+    )
+
+    @hybrid_property
+    def value(self):
+        return sum([c.current_price for c in self.cards if c.current_price])
+
+    @value.expression
+    def value(cls):
+        return select(func.sum([Card.current_price])).where(Card.collection_id == cls.id).as_scalar()
+ 
+    def value_history(self):
+        return select(func.sum(Price.price), Price.date).where(Price.card_id == self.id).order_by(Price.date.desc()).group_by(Price.date)
+
+    def __repr__(self):
+        return f"Collection {self.name} ({len(self.cards)} cards)"
+
+
 @event.listens_for(Price, "after_insert")
 def receive_after_insert(mapper, connection, price):
     "listen for the 'after_insert' event"
+    session = object_session(price)
     card = price.card
     collections = card.collections
-    for c in collections:
-        c.value = c.get_collection_value()
-        price.session.add(c)
-        price.session.commit()
-        print("Just Updated price for: ", c)
+    try:
+        session.begin_nested()
+        for c in collections:
+            c.value = c.get_collection_value()
+            print(f"{c} current value is {c.value}")
+            session.add(c)
+            print("Just Updated price for: ", c)
+        session.commit()
+        session.close()
+    except Exception as e:
+        session.rollback()
+        raise e
