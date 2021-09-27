239388d299b1d93d8d431691e42481bbcf2e430c
Stefan Schneider <mail@stefanschneider.me>
2021-09-27 20:54:05 +0200

    Clean up frontend code, add stats

--- frontend/src/views/dashboard/Home.vue
+++ frontend/src/views/dashboard/Home.vue
@@ -12,195 +12,39 @@
   </div>
 
   <div class="grid grid-cols-1 gap-4 px-4 mt-8 sm:grid-cols-4 sm:px-8">
-    <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
-      <div class="p-4 bg-green-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-12 w-12 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-      </div>
-      <div class="px-4 text-gray-700">
-        <h3 class="text-sm tracking-wider">Total Member</h3>
-        <p class="text-3xl">12,768</p>
-      </div>
-    </div>
-
-    <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
-      <div class="p-4 bg-blue-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-12 w-12 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-          />
-        </svg>
-      </div>
-      <div class="px-4 text-gray-700">
-        <h3 class="text-sm tracking-wider">Total Post</h3>
-        <p class="text-3xl">39,265</p>
-      </div>
-    </div>
-
-    <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
-      <div class="p-4 bg-indigo-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-12 w-12 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-          />
-        </svg>
-      </div>
-      <div class="px-4 text-gray-700">
-        <h3 class="text-sm tracking-wider">Total Comment</h3>
-        <p class="text-3xl">142,334</p>
-      </div>
-    </div>
-
-    <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
-      <div class="p-4 bg-red-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-12 w-12 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
-          />
-        </svg>
-      </div>
-      <div class="px-4 text-gray-700">
-        <h3 class="text-sm tracking-wider">Server Load</h3>
-        <p class="text-3xl">34.12%</p>
+    <div v-for="stat in stats" :key="stat.title">
+      <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+        <div :class="'p-4 bg-' + colors[Math.floor(Math.random() * colors.length)] + '-400'">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-12 w-12 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+            />
+          </svg>
+        </div>
+        <div class="px-4 text-gray-700">
+          <h3 class="text-sm tracking-wider">{{ stat.title }}</h3>
+          <p class="text-3xl">{{ stat.number }}</p>
+        </div>
       </div>
     </div>
   </div>
 
   <div class="grid grid-cols-1 px-4 gap-4 mt-8 sm:grid-cols-3 sm:px-8">
-    <div v-for="collection in collections" :key="collections.name">
+    <div v-for="collection in collections" :key="collection.name">
       <div class="px-4 py-2 bg-white border rounded-md overflow-hidden shadow">
         <h3 class="text-xl text-gray-600 mb-4">{{ collection.name }}</h3>
-        <apexchart type="area" :height="300" :options="pageViewOptions" :series="collection.value_history"></apexchart>
+        <apexchart type="area" :height="300" :options="chartOptions" :series="collection.value_history"></apexchart>
       </div>
     </div>
-    <!-- 
-    <div class="px-4 py-2 bg-white border rounded-md overflow-hidden shadow">
-      <h3 class="text-xl text-gray-600 mb-4">Sessions by device</h3>
-      <apexchart type="donut" :height="300" :options="sessionsOptions" :series="sessionsSeries"></apexchart>
-    </div>
-
-    <div class="grid grid-rows-2 gap-4">
-      <div class="px-4 py-2 bg-white border rounded-md shadow">
-        <h3 class="text-xl text-gray-600 mb-2">Popular Post</h3>
-
-        <div class="flex flex-col">
-          <div class="align-middle inline-block min-w-full">
-            <div class="overflow-hidden border-0">
-              <table class="table-auto min-w-full divide-y divide-gray-200">
-                <thead>
-                  <tr>
-                    <th scope="col" class="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Post Title
-                    </th>
-
-                    <th scope="col" class="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Views
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 text-sm text-gray-500">
-                  <tr>
-                    <td class="py-1">Create an Admin Panel with Vue.js and Tailwind CSS</td>
-                    <td class="py-1 text-sm text-gray-500">1,672</td>
-                  </tr>
-                  <tr>
-                    <td class="py-1">How To Center a Div</td>
-                    <td class="py-1 text-sm text-gray-500">1,423</td>
-                  </tr>
-                  <tr>
-                    <td class="py-1">Let's Star This Project</td>
-                    <td class="py-1 text-sm text-gray-500">926</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="px-4 py-2 bg-white border rounded-md shadow">
-        <h3 class="text-xl text-gray-600 mb-2">Top Contributor</h3>
-
-        <div class="flex flex-col">
-          <div class="align-middle inline-block min-w-full">
-            <div class="overflow-hidden border-0">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead>
-                  <tr>
-                    <th scope="col" class="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Name
-                    </th>
-
-                    <th scope="col" class="py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Commit
-                    </th>
-
-                    <th scope="col" class="py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Issue
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 text-sm text-gray-500">
-                  <tr>
-                    <td class="py-1 whitespace-nowrap">Seto Kuslaksono</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">41</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">12</td>
-                  </tr>
-                  <tr>
-                    <td class="py-1 whitespace-nowrap">Some Dude</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">22</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">15</td>
-                  </tr>
-                  <tr>
-                    <td class="py-1 whitespace-nowrap">It Could Be You</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">12</td>
-                    <td class="py-1 text-right whitespace-nowrap text-sm text-gray-500">4</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
@@ -208,8 +52,8 @@
 import VueApexCharts from 'vue3-apexcharts'
 import { ref } from 'vue'
 // import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-import topAuthors from '@/data/home/topAuthors.json'
-import recentSales from '@/data/home/recentSales.json'
+// import topAuthors from '@/data/home/topAuthors.json'
+// import recentSales from '@/data/home/recentSales.json'
 
 export default {
   components: {
@@ -232,9 +76,15 @@ export default {
     }
     getCollections()
 
-    console.log(collections)
+    const stats = ref([])
 
-    const pageViewOptions = {
+    async function getStats() {
+      const response = await fetch(`${host}/api/stats`)
+      stats.value = await response.json()
+    }
+    getStats()
+
+    const chartOptions = {
       chart: {
         id: 'pageview-chart',
         toolbar: {
@@ -246,33 +96,13 @@ export default {
       },
     }
 
-    const pageViewSeries = [
-      {
-        name: 'Page view',
-        data: [3087, 4403, 3456, 2864, 4956, 6023, 7063],
-      },
-    ]
-
-    const sessionsOptions = {
-      chart: {
-        id: 'sessions-chart',
-      },
-      labels: ['Desktop', 'Mobile', 'Tablet'],
-      legend: {
-        position: 'bottom',
-      },
-    }
-
-    const sessionsSeries = [70.9, 27.2, 1.9]
+    var colors = ['blue', 'green', 'red', 'yellow', 'indigo', 'purple']
 
     return {
-      pageViewOptions,
-      pageViewSeries,
-      sessionsOptions,
-      sessionsSeries,
-      topAuthors,
-      recentSales,
+      chartOptions,
+      stats,
       collections,
+      colors,
     }
   },
 }
